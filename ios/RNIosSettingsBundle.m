8cf8f6bcc22cd2cec580fab85adc57f2084d1b90
tk <tk@studio201.de>
2021-07-26 13:55:32 +0200

    load defaults from Settings.bundle

--- ios/RNIosSettingsBundle.m
+++ ios/RNIosSettingsBundle.m
@@ -12,13 +12,32 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(getValByKey:(NSString *)key  callback:(RCTResponseSenderBlock)callback)
 {
+    NSString *settingsBundle = [[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"];
+    if(!settingsBundle) {
+        NSLog(@"Could not find Settings.bundle");
+        return;
+    }
 
+    NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:[settingsBundle stringByAppendingPathComponent:@"Root.plist"]];
+    NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
+    NSMutableDictionary *defaultsToRegister = [[NSMutableDictionary alloc] initWithCapacity:[preferences count]];
+    for(NSDictionary *prefSpecification in preferences) {
+        NSString *key = [prefSpecification objectForKey:@"Key"];
+        if(key) {
+            [defaultsToRegister setObject:[prefSpecification objectForKey:@"DefaultValue"] forKey:key];
+        }
+    }
+
+    [[NSUserDefaults standardUserDefaults] registerDefaults:defaultsToRegister];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 
-    if([defaults objectForKey:key] == nil)
-        callback(@[ @[ @true ,@"value is empty or the key is not defined"] , [NSNull null]]);
+    if([defaults objectForKey:key] == nil){
+        NSString *result = [NSString stringWithFormat:@"value  is empty or the key %@ is not defined!!%@!!", key, [[NSUserDefaults standardUserDefaults] dictionaryRepresentation]];
+        callback(@[ @[ @true , result] , [NSNull null]]);
+    }
+
     else
-         callback(@[[NSNull null], [defaults objectForKey:key]]);
+        callback(@[[NSNull null], [defaults objectForKey:key]]);
 }
 
 @end
